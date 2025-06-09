25d8bb3e8fdf6a239826a67242de524f01305a0b
abbosaliy <abbosbekanvarjonov8@gmail.com>
2025-06-09 16:02:29 +0200

    add  Small Talk menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.scss';
+import ChatMenue from './talkMenue/talkMenue';
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <ChatMenue />,
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/talkMenue/talkMenue.tsx
@@ -0,0 +1,76 @@
+import './talkMenue.scss';
+
+function ChatMenue() {
+  return (
+    <div className="talk-container">
+      <div className="talk-header">
+        <div className="header-menue">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div className="talk-info">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+          necessitatibus quos tempora laudantium sed saepe, possimus eveniet
+          exercitationem a aspernatur vitae corrupti eligendi distinctio
+          suscipit consequuntur, blanditiis ab voluptatum ea!
+        </p>
+      </div>
+
+      <div className="talk-incon">
+        <div className="talk-icon_box">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+            />
+          </svg>
+        </div>
+        <div className="talk-icon_box">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+            />
+          </svg>
+        </div>
+        <div className="talk-icon_box">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ChatMenue;
